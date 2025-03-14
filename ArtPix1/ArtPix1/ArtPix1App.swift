ac2dd0d31f1fe5550a98e6529a2cb1c238a815be
Nilkanth14 <128330254+Nilkanth14@users.noreply.github.com>
2025-03-13 23:34:14 -0400

    Add files via upload

---
+++ ArtPix1/ArtPix1/ArtPix1App.swift
@@ -0,0 +1,17 @@
+//
+//  ArtPix1App.swift
+//  ArtPix1
+//
+//  Created by Nilkanth Amin on 2025-03-13.
+//
+
+import SwiftUI
+
+@main
+struct ArtPix1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtPix1/ArtPix1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArtPix1
+//
+//  Created by Nilkanth Amin on 2025-03-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
